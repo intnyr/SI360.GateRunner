5351cd0d2f222200ce6add9b8315d47e5d80394c
intnyr <86068743+intnyr@users.noreply.github.com>
2026-05-16 04:49:31 +0800

    Set VerticalAlignment=Top on search TextBoxes
    Add VerticalAlignment="Top" to the search TextBox in FlaUiScenarioMatrixView.xaml and TestScenarioMatrixView.xaml to ensure the search fields are aligned to the top of their grids. Remaining changes are rebuilt assembly/artifact updates from a project build.

--- obj/Debug/net8.0-windows/SI360.GateRunner.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SI360.GateRunner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SI360.GateRunner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900014626bf147b1d836b6b415d3bb89d85f4d5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3ec0b68b67926037c7cc4057f5391711fbc8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("SI360.GateRunner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SI360.GateRunner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
